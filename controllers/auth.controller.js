b53a4ad29f37142ac43714f392d7e64fc8cb6bce
Jophin Babu <jophin735@gmail.com>
2026-02-02 12:40:25 +0530

    newwww

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -46,7 +46,7 @@ export const signup = async (req, res) => {
 
     if (newUser) {
       // Generate JWT token and set as cookie
-      generateToken(newUser._id, res);
+      const token = generateToken(newUser._id, res);
       await newUser.save();
 
       res.status(201).json({
@@ -54,6 +54,7 @@ export const signup = async (req, res) => {
         fullName: newUser.fullName,
         email: newUser.email,
         profilePic: newUser.profilePic,
+        token,
       });
     } else {
       res.status(400).json({ message: "Invalid user data" });
@@ -81,13 +82,14 @@ export const login = async (req, res) => {
     }
 
     // 3. Generate JWT and set as cookie
-    generateToken(user._id, res);
+    const token = generateToken(user._id, res);
 
     res.status(200).json({
       _id: user._id,
       fullName: user.fullName,
       email: user.email,
       profilePic: user.profilePic,
+      token,
     });
   } catch (error) {
     console.error("Error in login controller:", error.message);
